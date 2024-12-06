be21ad3807712c203b97c036ca1edfc8c851defc
Niklas Trapp <niklas031712@gmail.com>
2024-12-06 11:44:58 +0100

    idk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'learn.ionic',
-  appName: 'learn-ionic',
+  appId: 'io.ionic.starter',
+  appName: 'ionic-app-base',
   webDir: 'www'
 };
 
