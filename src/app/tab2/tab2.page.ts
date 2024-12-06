9b123fd127fa57b43761656cf49c177e1f57e17e
Niklas Trapp <niklas031712@gmail.com>
2024-12-06 14:16:30 +0100

    add tab to display device infos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,65 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+import { BatteryInfo, Device } from '@capacitor/device';
+import { ConnectionStatus, ConnectionType, Network } from '@capacitor/network';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonicModule]
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  constructor(private ngZone: NgZone) {
+  }
+
+  battery: number | undefined = undefined
+  loading: string | undefined = undefined
+
+  networkConnected: boolean | undefined = undefined
+  networkType: string | undefined = undefined
+
+  Math = Math;
+
+  async reload(e: any) {
+    this.battery = (await Device.getBatteryInfo()).batteryLevel
+    this.loading = (await Device.getBatteryInfo()).isCharging ? "Loading" : "Not loading"
+    this.networkType = (await Network.getStatus()).connectionType
+    this.networkConnected = (await Network.getStatus()).connected
+
+    e ? e.target.complete() : null
+  }
+
+  async getBadgeColor() {
+    return "warning"
+  }
+
+  getNetTypeIcon() {
+    switch (this.networkType) {
+      case "wifi":
+        return "wifi-outline"
+      case "cellular":
+        return "cellular-outline"
+      case "unknown":
+        return "help-circle-outline"
+      default:
+        return "close-circle-outline"
+    }
+  }
+
+  ngOnInit() {
+    this.reload(null);
+
+    Network.addListener('networkStatusChange', status => {
+      this.ngZone.run(() => {
+        this.networkConnected = status.connected
+        this.networkType = status.connectionType
+        console.log("status", status.connected)
+      })
+    })
+  }
 
 }
